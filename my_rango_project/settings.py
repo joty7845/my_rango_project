b71a774f9836751185ec3c33a4e3c0dc13a5f8e5
Joseph <wz121b96ap@outlook.com>
2025-02-13 01:39:29 +0000

    Verified database population and Category display

--- my_rango_project/settings.py
+++ my_rango_project/settings.py
@@ -108,6 +108,22 @@ INSTALLED_APPS = [
     'rango',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # 添加这个路径
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
+    category_list = Category.objects.all()
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
+
+
