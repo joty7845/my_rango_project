a0f8b35cd91348e4688d10a0c11f74aedba11781
Joseph <wz121b96ap@outlook.com>
2025-02-13 21:54:14 +0000

    Final submission: Completed all Rango project requirements, including about page and category listing

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,6 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
 
--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-from rango.models import Category
+from django.shortcuts import render, get_object_or_404
+from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.all()
     context_dict = {'categories': category_list}
     return render(request, 'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug):
+    category = get_object_or_404(Category, slug=category_name_slug)
+    pages = Page.objects.filter(category=category)
+
+    context_dict = {
+        'category': category,
+        'pages': pages
+    }
+    return render(request, 'rango/category.html', context_dict)
+
+
+
 
 
